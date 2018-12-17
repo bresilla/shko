fa0d84d94d54da2edead9f65f23c6e1d5b539a77
Trim Bresilla <trim.bresilla@gmail.com>
2018-12-17 15:08:15 +0100

    selecting finished

--- src/spejt.go
+++ src/spejt.go
@@ -9,21 +9,19 @@ import (
 )
 
 var (
-	incFolder  = true
-	incFiles   = true
-	incHidden  = true
-	currentDir = StringDirToFile(os.Getenv("PWD"))
+	incFolder       = true
+	incFiles        = true
+	incHidden       = false
+	cursoroll       = true
+	cursorset       = false
+	shortcut   rune = 113
+	currentDir      = StringDirToFile(os.Getenv("PWD"))
 	parent     string
 	child      string
 	cmd        *exec.Cmd
 	startNr    = 0
+	cursorarr  []int
 )
-
-func MakeCool(file []File) {
-	for _, f := range file {
-		fmt.Println(f.Name)
-	}
-}
 
 func ChangeDir(cmd *exec.Cmd, filepath string) {
 	cmd = exec.Command("cd", filepath)
@@ -43,31 +41,51 @@ keyPressListenerLoop:
 		SelectInList(startNr, children)
 		switch ev := term.PollEvent(); ev.Type {
 		case term.EventKey:
-			if ev.Key == term.KeySpace {
-				fmt.Println("SPACE")
+			if ev.Ch == shortcut {
+				break keyPressListenerLoop
 			} else if ev.Key == term.KeyEsc || ev.Ch == 45 {
 				break keyPressListenerLoop
 			} else if ev.Key == term.KeyArrowUp {
 				term.Sync()
 				startNr--
 				if startNr < 0 {
-					startNr = len(children) - 1
+					if cursoroll {
+						startNr = len(children) - 1
+					} else {
+						startNr = 0
+					}
 				}
 			} else if ev.Key == term.KeyArrowDown {
 				term.Sync()
 				startNr++
 				if startNr > len(children)-1 {
-					startNr = 0
+					if cursoroll {
+						startNr = 0
+					} else {
+						startNr = len(children) - 1
+					}
 				}
 			} else if ev.Key == term.KeyArrowLeft {
 				term.Sync()
-				startNr = 0
+				if len(cursorarr) > 0 && !cursorset {
+					startNr = cursorarr[len(cursorarr)-1]
+					cursorarr = cursorarr[:len(cursorarr)-1]
+				} else {
+					startNr = 0
+				}
 				currentDir = CurrentDir(parent.Path)
 			} else if ev.Key == term.KeyArrowRight {
+				cursorarr = append(cursorarr, startNr)
 				term.Sync()
+				if children[startNr].IsDir {
+					currentDir = CurrentDir(children[startNr].Path)
+				} else {
+					OpenInEditor(children[startNr].Path)
+				}
+				startNr = 0
 			} else {
 				term.Sync()
-				//fmt.Println("ASCII : ", ev.Ch)
+				fmt.Println("ASCII : ", ev.Ch)
 			}
 		case term.EventError:
 			panic(ev.Err)
--- src/spejt_select.go
+++ src/spejt_select.go
@@ -1,13 +1,38 @@
 package spejt
 
+var (
+	active     string
+	deactive   string
+	ttyW, ttyH = Tty_Size()
+)
+
 func SelectInList(selected int, file []File) {
 	for num, el := range file {
-		if num < selected {
-			Print(Default, None, None, el.Name)
-		} else if num == selected {
-			Print(HighLight, Red, None, el.Name)
-		} else if num > selected {
-			Print(Default, None, None, el.Name)
+		if el.IsDir {
+			active = "\t »   " + el.Name + "/"
+			deactive = "\t»   " + el.Name + "/"
+		} else {
+			active = "\t ≡   " + el.Name
+			deactive = "\t≡   " + el.Name
 		}
+		if num < selected {
+			colorList(el, false)
+		} else if num == selected {
+			colorList(el, true)
+		} else if num > selected {
+			colorList(el, false)
+		}
+	}
+}
+
+func colorList(file File, active bool) {
+	if file.IsDir && active {
+		Print(HighLight, Red, None, "\t ≡   "+file.Name)
+	} else if file.IsDir && !active {
+		Print(Default, None, None, "\t≡   "+file.Name)
+	} else if !file.IsDir && active {
+		Print(HighLight, Red, None, "\t »   "+file.Name)
+	} else if !file.IsDir && !active {
+		Print(Default, None, None, "\t»   "+file.Name)
 	}
 }
